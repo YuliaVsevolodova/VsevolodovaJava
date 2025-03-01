0973d42846c0ef09f67d421f228b3e33fe8ff9e6
v.ilyinets <v.ilyinets@astondevs.ru>
2025-03-01 17:41:34 +0300

    Java Task Aston

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 public class Main {
 public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+
     System.out.print("Введите число 'a': ");
     int a = scanner.nextInt();
     System.out.print("Введите число 'b': ");
     int b = scanner.nextInt();
+
     compareNumbers(a, b);
     mathOperations(a, b);
+
     scanner.nextLine();
+
     System.out.print("Введите первую строку: ");
     String c = scanner.nextLine();
+
     System.out.print("Введите вторую строку: ");
     String d = scanner.nextLine();
+
     stringComparison(c, d);
+
     int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     oddNumbers(numbers);
 }
@@ -47,14 +54,9 @@ public static void stringComparison(String c, String d) {
 }
 
 public static void oddNumbers(int[] numbers) {
-    System.out.println("Четные числа в массиве: ");
-    int[] var1 = numbers;
-    int var2 = numbers.length;
-
-    for(int var3 = 0; var3 < var2; ++var3) {
-        int number = var1[var3];
+    for (int number : numbers) {
         if (number % 2 == 0) {
-            System.out.println(number);
+            System.out.print(number + " ");
         }
     }
 }
